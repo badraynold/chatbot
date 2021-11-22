00b0548f4869502c42c05b00574af77974d4cb44
Rafał Łukawski <rs.lukawski@gmail.com>
2021-11-22 11:12:25 +0100

    chatbot lines and conversations

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,24 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 22 10:12:27 2021
+# Bouilding a ChatBot with Deep NLP
 
-@author: rafal
-"""
+import numpy as np
+import tensorflow as tf
+import re
+import time
 
+# Importing the dataset
 
+lines = open('movie_lines.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+conversations = open('movie_conversations.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+
+id2line = {}
+for line in lines:
+    _line =  line.split(' +++$+++ ')
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4]
+
+conversations_ids = []
+for conversation in conversations[:-1]:
+    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'",'')
+    conversations_ids.append(_conversation.split(','))
+    
+    
