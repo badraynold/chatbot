be1de596002e45845b06e5e7ce6865733f80ac83
Rafał Łukawski <rs.lukawski@gmail.com>
2021-11-23 12:06:37 +0100

    clean text

--- chatbot.py
+++ chatbot.py
@@ -31,4 +31,29 @@ for conversation in conversations_ids:
         answers.append(id2line[conversation[i+1]])
         
         
+# cleaning of the texts
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"won't", " will not", text)
+    text = re.sub(r"can't", " can not", text)
+    text = re.sub(r"[-()\"#/@;:<>{}+=~|.?,]", "", text)
+    return text
+
+    
+    
+    
+    
+    
+    
     
